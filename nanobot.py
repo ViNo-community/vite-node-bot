22f3427abcede6c81058733ed895cb05d4fc3af6
JohnGarnham <john.e.garnham@gmail.com>
2021-05-21 04:19:39 -0600

    Add account_balance and account_info helper functions

--- nanobot.py
+++ nanobot.py
@@ -4,16 +4,41 @@ import asyncio
 import websockets
 import json
 
+# Nano node RPC document: https://docs.nano.org/commands/rpc-protocol/
+
+# Send RPC to node
+def send(method, params): 
+    print("Method: " + method + " Params: " + params)
+
+# Get account balance information for given account
+def account_balance(account):
+    message = {
+        "action": "account_balance",
+        "account": account
+    }
+    return json.dumps(message)
+
+# Get account info for given account
+def account_info(account):
+    message = {
+        "action": "account_info",
+        "account": account
+    }
+    return json.dumps(message)
+    
+def subscription():
+    message = {
+        'action': 'subscribe',
+        'topic': 'confirmation',
+        'ack': True
+    } 
+    return json.dumps(message)
+
 async def connect():
     uri = "wss://ws.mynano.ninja"
     async with websockets.connect(uri) as websocket:
-        params = {
-            'action': 'subscribe',
-            'topic': 'confirmation',
-            'ack': True
-        }
-
-        await websocket.send(json.dumps(params))
+        # Send acknowledgement
+        await websocket.send(subscription())
         while True:
             answer = await websocket.recv()
             answer = json.loads(answer)
