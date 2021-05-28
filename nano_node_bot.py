03f52065f479ef1dc4a05186110f321515c3026f
JohnGarnham <john.e.garnham@gmail.com>
2021-05-28 17:46:11 -0600

    Add logging func for debug. May remove later.

--- nano_node_bot.py
+++ nano_node_bot.py
@@ -24,21 +24,28 @@ DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 DISCORD_GUILD = os.getenv('DISCORD_GUILD')
 RPC_URL = os.getenv('RPC_URL')
 
+# Helper function for logging
+# {User} requests {command}
+def logit(ctx):
+    print(f"{ctx.message.author} requests {ctx.command}")
+    logging.info(f"{ctx.message.author} requests {ctx.command}")
+
 # Initiate Discord bot
 bot = commands.Bot(command_prefix='.')
 
 # Bot is ready
 @bot.event
 async def on_ready():
-    logging.info("Bot connected")
-    print(f"Bot connected. Log file:", logfile)
+    # Log successful connection
+    logging.info(f"{bot.user.name} connected")
+    print(f"{bot.user.name} connected. Log file:", logfile)
 
 # Helper function for getting value from JSON response
-async def get_value(param):
+async def get_value(ctx, param):
     answer = ""
     try:
         # Log query
-        logging.info(f"Request for: {param}")
+        logit(ctx)
         # Grab response from RPC_URL
         r = requests.get(RPC_URL, timeout=2.50)
         # Parse JSON
@@ -54,13 +61,13 @@ async def get_value(param):
 
 @bot.command(name='version', help="Displays node version")
 async def version(ctx):
-    value = await get_value('version')
+    value = await get_value(ctx,'version')
     response = f"Node version is {value}"
     await ctx.send(response)
 
 @bot.command(name='uptime', help="Displays node uptime")
 async def uptime(ctx):
-    value = await get_value('nodeUptimeStartup')
+    value = await get_value(ctx,'nodeUptimeStartup')
     time = int(value)
     # Break down into days, hours, minutes, seconds
     day = time // (24 * 3600)
@@ -75,31 +82,31 @@ async def uptime(ctx):
 
 @bot.command(name='server_uptime', help="Displays server uptime")
 async def server_uptime(ctx):
-    value = await get_value('systemUptime')
+    value = await get_value(ctx,'systemUptime')
     response = f"Server uptime is {value}"
     await ctx.send(response)
 
 @bot.command(name='voting_weight', help="Displays voting weight")
 async def voting_weight(ctx):
-    value = await get_value('votingWeight')
+    value = await get_value(ctx,'votingWeight')
     response = f"Voting weight is {value:.2f} nano"
     await ctx.send(response)
 
 @bot.command(name='current_block', help="Displays the current block")
 async def current_block(ctx):
-    value = await get_value('currentBlock')
+    value = await get_value(ctx,'currentBlock')
     response = f"Current block is {value}"
     await ctx.send(response)
 
 @bot.command(name='cemented_blocks', help="Displays the cemented block count")
 async def cemented_blocks(ctx):
-    value = await get_value('cementedBlocks')
+    value = await get_value(ctx,'cementedBlocks')
     response = f"Cemented block count is {value}"
     await ctx.send(response)
 
 @bot.command(name='sync', help="Displays block sync")
 async def block_sync(ctx):
-    value = await get_value('blockSync')
+    value = await get_value(ctx,'blockSync')
     response = f"Block sync is {value}%"
     await ctx.send(response)
 
