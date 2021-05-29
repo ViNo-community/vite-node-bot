b9bd3b9de220347c5cdbef9ef4e150e8ac7e4f45
JohnGarnham <john.e.garnham@gmail.com>
2021-05-29 01:21:24 -0600

    Add common.py for common util functions

---
+++ common.py
@@ -0,0 +1,67 @@
+import os
+from dotenv import load_dotenv
+import requests
+import logging
+import json
+import datetime
+from pathlib import Path
+
+class Common():
+
+    load_dotenv()
+    discord_token= os.getenv('DISCORD_TOKEN')
+    rpc_url = os.getenv('RPC_URL')
+    filename = datetime.datetime.now().strftime("%Y%m%d%H%M%S") + "_nano_node_bot.log"
+    logdir = Path(__file__).resolve().parent / "logs" 
+    # Make directory if it doesn't already exist
+    if not os.path.exists(logdir):
+        try:
+            os.makedirs(logdir)
+        except OSError as e:
+            print(f"Error creating {logdir} :", e)
+            exit()
+    logfile = logdir / filename
+    logging.basicConfig(filename=logfile, format='%(asctime)-10s - %(levelname)s - %(message)s', level=logging.INFO)
+    logger = logging.getLogger(__name__)
+
+    def __init__(self):
+        pass
+        
+    def get_discord_token(self):
+        return self.discord_token
+
+    def get_rpc_url(self):
+        return self.rpc_url
+
+    # Helper function for generic logging
+    @staticmethod
+    def log(msg):
+        Common.logger.info(msg)
+
+    # Helper function for logging bot commands
+    # <- {User} : {command}
+    @staticmethod
+    def logit(ctx):
+        print(f"-> {ctx.message.author} : {ctx.command}")
+        Common.logger.info(f"-> {ctx.message.author} : {ctx.command}")
+        
+    # Helper function for getting value from JSON response
+    @staticmethod
+    async def get_value(ctx, param):
+        answer = ""
+        try:
+            # Log query
+            Common.logit(ctx)
+            # Grab response from RPC_URL
+            r = requests.get(Common.rpc_url, timeout=2.50)
+            # Parse JSON
+            content = json.loads(r.text)
+            # Grab value named param
+            answer = content[param]
+            # Log answer 
+            print(f"<- {answer}")
+            Common.logger.info(f"<- {answer}")
+        except Exception as ex:
+            # Log exception with stack trace. 
+            Common.logger.error("Exception occured", exc_info=True)
+        return answer
--- nano_node_bot.py
+++ nano_node_bot.py
@@ -6,36 +6,15 @@ import discord
 import requests
 import logging
 import datetime
-from config import Config
+from common import Common
 from pathlib import Path
 from discord.ext import commands
 
 # Nano node RPC document: https://docs.nano.org/commands/rpc-protocol/
 
-# Set up logging
-filename = datetime.datetime.now().strftime("%Y%m%d%H%M%S") + "_nano_node_bot.log"
-logdir = Path(__file__).resolve().parent / "logs" 
-# Make directory if it doesn't already exist
-if not os.path.exists(logdir):
-    try:
-        os.makedirs(logdir)
-    except OSError as e:
-        print(f"Error creating {logdir} :", e)
-        exit()
-logfile = logdir / filename
-logging.basicConfig(filename=logfile, format='%(asctime)-10s - %(levelname)s - %(message)s', level=logging.INFO)
-logger = logging.getLogger(__name__)
-
 # Load discord token from .env file
-config = Config()
-DISCORD_TOKEN = config.get_discord_token()
-RPC_URL = config.get_rpc_url()
-
-# Helper function for logging
-# {User} requests {command}
-def logit(ctx):
-    print(f"{ctx.message.author} requests {ctx.command}")
-    logger.info(f"{ctx.message.author} requests {ctx.command}")
+common = Common()
+DISCORD_TOKEN = common.get_discord_token()
 
 # Initiate Discord bot
 bot = commands.Bot(command_prefix='.')
@@ -44,46 +23,26 @@ bot = commands.Bot(command_prefix='.')
 @bot.event
 async def on_ready():
     # Log successful connection
-    logger.info(f"{bot.user.name} connected")
-    print(f"{bot.user.name} connected. Log file:", logfile)
-
-# Helper function for getting value from JSON response
-async def get_value(ctx, param):
-    answer = ""
-    try:
-        # Log query
-        logit(ctx)
-        # Grab response from RPC_URL
-        r = requests.get(RPC_URL, timeout=2.50)
-        # Parse JSON
-        content = json.loads(r.text)
-        # Grab value named param
-        answer = content[param]
-        # Log answer
-        logger.info(f"Answer: {answer}")
-    except Exception as ex:
-        # Log exception with stack trace
-        logger.error("Exception occured", exc_info=True)
-    return answer
+    Common.log(f"{bot.user.name} connected")
 
 # TODO: Put into Node cog - address, version, uptime, server_uptime
 ####
 
 @bot.command(name='address', aliases=['addr','node_address','nodeaddress'], help="Displays node address")
 async def address(ctx):
-    value = await get_value(ctx,'nanoNodeAccount')
+    value = await Common.get_value(ctx,'nanoNodeAccount')
     response = f"Node address is {value}"
     await ctx.send(response)
 
 @bot.command(name='version', aliases=['ver'], help="Displays node version")
 async def version(ctx):
-    value = await get_value(ctx,'version')
+    value = await Common.get_value(ctx,'version')
     response = f"Node version is {value}"
     await ctx.send(response)
 
 @bot.command(name='uptime', aliases=['up','nodeuptime','node_uptime'], help="Displays node uptime")
 async def uptime(ctx):
-    value = await get_value(ctx,'nodeUptimeStartup')
+    value = await Common.get_value(ctx,'nodeUptimeStartup')
     time = int(value)
     # Break down into days, hours, minutes, seconds
     day = time // (24 * 3600)
@@ -98,28 +57,28 @@ async def uptime(ctx):
 
 @bot.command(name='server_uptime', aliases=['serveruptime','sup'], help="Displays server uptime")
 async def server_uptime(ctx):
-    value = await get_value(ctx,'systemUptime')
+    value = await Common.get_value(ctx,'systemUptime')
     response = f"Server uptime is {value}"
     await ctx.send(response)
 
 @bot.command(name='server_load', aliases=['serverload','systemload','load'], help="Displays server load")
 async def server_load(ctx):
-    value = await get_value(ctx,'systemLoad')
+    value = await Common.get_value(ctx,'systemLoad')
     response = f"Server load is {value}"
     await ctx.send(response)
 
 #TODO: Double-check units for usedMem, totalMem values. Guessing it's MB. 
 @bot.command(name='mem_usage', aliases=['memory_usage','memusage','memory','mem'], help="Displays memory usage")
 async def mem_usage(ctx):
-    usedMem = await get_value(ctx,'usedMem')
-    totalMem = await get_value(ctx,'totalMem')
+    usedMem = await Common.get_value(ctx,'usedMem')
+    totalMem = await Common.get_value(ctx,'totalMem')
     percent = int(usedMem) / int(totalMem) * 100
     response = f"Memory usage is {usedMem} MB / {totalMem} MB : {percent:.2f}%"
     await ctx.send(response)
 
 @bot.command(name='node_name', aliases=['nodename','hostname','host','name','node'], help="Displays node name")
 async def node_name(ctx):
-    value = await get_value(ctx,'nanoNodeName')
+    value = await Common.get_value(ctx,'nanoNodeName')
     response = f"Node hostname is {value}"
     await ctx.send(response)
 ####
@@ -129,25 +88,25 @@ async def node_name(ctx):
 
 @bot.command(name='balance', aliases=['bal','show_balance'], help="Displays account balance")
 async def balance(ctx):
-    value = await get_value(ctx,'accBalanceMnano')
+    value = await Common.get_value(ctx,'accBalanceMnano')
     response = f"Account balance is {value:.2f} nano"
     await ctx.send(response)
 
 @bot.command(name='representative', aliases=['rep','show_rep','show_representative'], help="Displays representative")
 async def representative(ctx):
-    value = await get_value(ctx,'repAccount')
+    value = await Common.get_value(ctx,'repAccount')
     response = f"Representative: {value}"
     await ctx.send(response)
 
 @bot.command(name='num_peers', aliases=['numpeers','peers'], help="Displays number of peers")
 async def num_peers(ctx):
-    value = await get_value(ctx,'numPeers')
+    value = await Common.get_value(ctx,'numPeers')
     response = f"{value} peers"
     await ctx.send(response)
 
 @bot.command(name='voting_weight', aliases=['votingweight','weight','voting'], help="Displays voting weight")
 async def voting_weight(ctx):
-    value = await get_value(ctx,'votingWeight')
+    value = await Common.get_value(ctx,'votingWeight')
     response = f"Voting weight is {value:.2f} nano"
     await ctx.send(response)
 
@@ -157,25 +116,25 @@ async def voting_weight(ctx):
 
 @bot.command(name='current_block', aliases=['currentblock','cur','current'], help="Displays the current block")
 async def current_block(ctx):
-    value = await get_value(ctx,'currentBlock')
+    value = await Common.get_value(ctx,'currentBlock')
     response = f"Current block is {value}"
     await ctx.send(response)
 
 @bot.command(name='cemented_blocks', aliases=['cementedblocks','cemented','cem'], help="Displays the cemented block count")
 async def cemented_blocks(ctx):
-    value = await get_value(ctx,'cementedBlocks')
+    value = await Common.get_value(ctx,'cementedBlocks')
     response = f"Cemented block count is {value}"
     await ctx.send(response)
 
 @bot.command(name='unchecked_blocks',  aliases=['uncheckedblocks','unchecked'], help="Displays the number of unchecked blocks")
 async def unchecked_blocks(ctx):
-    value = await get_value(ctx,'uncheckedBlocks')
+    value = await Common.get_value(ctx,'uncheckedBlocks')
     response = f"{value} unchecked blocks"
     await ctx.send(response)
 
 @bot.command(name='sync', aliases=['blocksync','block_sync','bsync'], help="Displays block sync")
 async def block_sync(ctx):
-    value = await get_value(ctx,'blockSync')
+    value = await Common.get_value(ctx,'blockSync')
     response = f"Block sync is {value}%"
     await ctx.send(response)
 
@@ -185,7 +144,7 @@ async def block_sync(ctx):
 @bot.event
 async def on_command_error(ctx, error):
     print(f"{ctx.message.author} tried {ctx.invoked_with} Error: ", error)
-    logger.info(f"{ctx.message.author} tried {ctx.invoked_with} Error: {error}")
+    Common.log(f"{ctx.message.author} tried {ctx.invoked_with} Error: {error}")
 
 # Run the bot
 bot.run(DISCORD_TOKEN)
