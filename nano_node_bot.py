1f41107939c4a9dea2662946233a92c1366dd957
JohnGarnham <john.e.garnham@gmail.com>
2021-05-28 20:55:06 -0600

    Add aliases for commands

--- nano_node_bot.py
+++ nano_node_bot.py
@@ -70,13 +70,13 @@ async def get_value(ctx, param):
 # TODO: Put into Node cog - version, uptime, server_uptime
 ####
 
-@bot.command(name='version', help="Displays node version")
+@bot.command(name='version', aliases=['ver'], help="Displays node version")
 async def version(ctx):
     value = await get_value(ctx,'version')
     response = f"Node version is {value}"
     await ctx.send(response)
 
-@bot.command(name='uptime', help="Displays node uptime")
+@bot.command(name='uptime', aliases=['up','nodeuptime','node_uptime'], help="Displays node uptime")
 async def uptime(ctx):
     value = await get_value(ctx,'nodeUptimeStartup')
     time = int(value)
@@ -91,27 +91,28 @@ async def uptime(ctx):
     response = f"Node uptime is {day} days, {hours} hours, {minutes} minutes, and {seconds} seconds"
     await ctx.send(response)
 
-@bot.command(name='server_uptime', help="Displays server uptime")
+@bot.command(name='server_uptime', aliases=['serveruptime','sup'], help="Displays server uptime")
 async def server_uptime(ctx):
     value = await get_value(ctx,'systemUptime')
     response = f"Server uptime is {value}"
     await ctx.send(response)
 
-@bot.command(name='server_load', help="Displays server load")
+@bot.command(name='server_load', aliases=['serverload','systemload','load'], help="Displays server load")
 async def server_load(ctx):
     value = await get_value(ctx,'systemLoad')
     response = f"Server load is {value}"
     await ctx.send(response)
 
-@bot.command(name='mem_usage', help="Displays memory usage")
+#TODO: Double-check units for usedMem, totalMem values. Guessing it's MB. 
+@bot.command(name='mem_usage', aliases=['memory_usage','memusage','memory','mem'], help="Displays memory usage")
 async def mem_usage(ctx):
     usedMem = await get_value(ctx,'usedMem')
     totalMem = await get_value(ctx,'totalMem')
     percent = int(usedMem) / int(totalMem) * 100
-    response = f"Memory usage is {usedMem} / {totalMem} : {percent:.2f}%"
+    response = f"Memory usage is {usedMem} MB / {totalMem} MB : {percent:.2f}%"
     await ctx.send(response)
 
-@bot.command(name='node_name', help="Displays node name")
+@bot.command(name='node_name', aliases=['nodename','hostname','host','name','node'], help="Displays node name")
 async def node_name(ctx):
     value = await get_value(ctx,'nanoNodeName')
     response = f"Node hostname is {value}"
@@ -121,54 +122,53 @@ async def node_name(ctx):
 # TODO: Put into Accounts cog - balance, representative, num_peers, voting_weight
 ####
 
-@bot.command(name='balance', help="Displays account balance")
+@bot.command(name='balance', aliases=['bal','show_balance'], help="Displays account balance")
 async def balance(ctx):
     value = await get_value(ctx,'accBalanceMnano')
     response = f"Account balance is {value:.2f} nano"
     await ctx.send(response)
 
-@bot.command(name='representative', help="Displays representative")
+@bot.command(name='representative', aliases=['rep','show_rep','show_representative'], help="Displays representative")
 async def representative(ctx):
     value = await get_value(ctx,'repAccount')
     response = f"Representative: {value}"
     await ctx.send(response)
 
-@bot.command(name='num_peers', help="Displays number of peers")
+@bot.command(name='num_peers', aliases=['numpeers','peers'], help="Displays number of peers")
 async def num_peers(ctx):
     value = await get_value(ctx,'numPeers')
     response = f"{value} peers"
     await ctx.send(response)
 
-@bot.command(name='voting_weight', help="Displays voting weight")
+@bot.command(name='voting_weight', aliases=['votingweight','weight','vweight'], help="Displays voting weight")
 async def voting_weight(ctx):
     value = await get_value(ctx,'votingWeight')
     response = f"Voting weight is {value:.2f} nano"
     await ctx.send(response)
 
 ####
-
 #TODO: Put into Blocks cog - current_block, cemented_blocks, unchecked_blocks, sync
 ###
 
-@bot.command(name='current_block', help="Displays the current block")
+@bot.command(name='current_block', aliases=['currentblock','cur','current'], help="Displays the current block")
 async def current_block(ctx):
     value = await get_value(ctx,'currentBlock')
     response = f"Current block is {value}"
     await ctx.send(response)
 
-@bot.command(name='cemented_blocks', help="Displays the cemented block count")
+@bot.command(name='cemented_blocks', aliases=['cementedblocks','cemented','cem'], help="Displays the cemented block count")
 async def cemented_blocks(ctx):
     value = await get_value(ctx,'cementedBlocks')
     response = f"Cemented block count is {value}"
     await ctx.send(response)
 
-@bot.command(name='unchecked_blocks', help="Displays the number of unchecked blocks")
+@bot.command(name='unchecked_blocks',  aliases=['uncheckedblocks','unchecked'], help="Displays the number of unchecked blocks")
 async def unchecked_blocks(ctx):
     value = await get_value(ctx,'uncheckedBlocks')
     response = f"{value} unchecked blocks"
     await ctx.send(response)
 
-@bot.command(name='sync', help="Displays block sync")
+@bot.command(name='sync', aliases=['blocksync','block_sync','bsync'], help="Displays block sync")
 async def block_sync(ctx):
     value = await get_value(ctx,'blockSync')
     response = f"Block sync is {value}%"
