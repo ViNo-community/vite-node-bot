6acb32a473e4fe50ff564d1153dcca5cba3170ea
JohnGarnham <john.e.garnham@gmail.com>
2021-06-04 02:03:34 -0600

    Make NanoNodeBot a class

--- nano_node_bot.py
+++ nano_node_bot.py
@@ -12,47 +12,53 @@ from bots import BotCog
 from common import Common
 
 # Nano node RPC document: https://docs.nano.org/commands/rpc-protocol/
-
-# Load discord token from .env file
 common = Common()
 DISCORD_TOKEN = common.get_discord_token()
-prefix = common.get_command_prefix()
-print("Command prefix ", prefix)
+
+class NanoNodeBot(commands.Bot):
+
+    def __init__(self):
+        # Load discord token from .env file
+        common = Common()
+        DISCORD_TOKEN = common.get_discord_token()
+        prefix = common.get_command_prefix()
+        print("Command prefix ", prefix)
+        commands.Bot.__init__(self, command_prefix=prefix,description="Nano Node Bot")
+        # Add cogs
+        self.add_cog(BlocksCog(self))
+        self.add_cog(AccountsCog(self))
+        self.add_cog(NodesCog(self))
+        self.add_cog(ServerCog(self))
+        self.add_cog(BotCog(self))
+    
+    # This is called when the bot has logged on and set everything up
+    async def on_ready(self):
+        # Log successful connection
+        print("Bot connected")
+        Common.log(f"{bot.user.name} connected")
+        #node_name = await Common.get_value(ctx,'nanoNodeName')
+        await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='Nano Node'))
+
+    # This is called when the bot sees an unknown command
+    async def on_command_error(self, ctx, error):
+        Common.log_error(f"{ctx.message.author} tried unknown command {ctx.invoked_with} Error: {error}")
+        await ctx.send(f"I do not know what {ctx.invoked_with} means.")
+
+    # This is called when the bot has an error
+    async def on_error(self, ctx, error):
+        print("Bot encountered error: ", error)
+        Common.log_error("Error: {error}")
+
+    # This is called when the bot disconnects
+    async def on_disconnect(self):
+        print("Bot disconnected")
+        # Log successful connection
+        Common.log_error(f"{bot.user.name} disconnected.")
 
 # Initiate Discord bot
-bot = commands.Bot(command_prefix=prefix)
+bot = NanoNodeBot()
 
-# This is called when the bot has logged on and set everything up
-@bot.event
-async def on_ready():
-    # Log successful connection
-    Common.log(f"{bot.user.name} connected")
-    #node_name = await Common.get_value(ctx,'nanoNodeName')
-    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='Nano Node'))
 
-# Error. Log.
-@bot.event
-async def on_error(ctx, error):
-    Common.log_error("Error: {error}")
-
-# Command not found. Tell user command does not exist and then log it. 
-@bot.event
-async def on_command_error(ctx, error):
-    Common.log_error(f"{ctx.message.author} tried unknown command {ctx.invoked_with} Error: {error}")
-    await ctx.send(f"I do not know what {ctx.invoked_with} means")
-
-@bot.event
-async def on_disconnect():
-    print("Bot disconnected")
-    # Log successful connection
-    Common.log_error(f"{bot.user.name} disconnected.")
-
-# Add cogs
-bot.add_cog(BlocksCog(bot))
-bot.add_cog(AccountsCog(bot))
-bot.add_cog(NodesCog(bot))
-bot.add_cog(ServerCog(bot))
-bot.add_cog(BotCog(bot))
 
 # Run the bot
 bot.run(DISCORD_TOKEN)
