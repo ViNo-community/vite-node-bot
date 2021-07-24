5f70c8d3cf55e2396690c480ad9bdf79bbdc2acf
John Garnham <john.e.garnham@gmail.com>
2021-07-24 00:19:41 -0600

    Add common date/time functions

---
+++ common.ts
@@ -0,0 +1,23 @@
+import {DateTime} from 'luxon';
+
+export const getLatestCycleTimestampFromNow = () => {
+    const nowUtc = DateTime.utc();
+    const midnightUtc = nowUtc.set({second: 0, minute: 0, hour: 0, millisecond: 0});
+    return midnightUtc.toSeconds();
+};
+
+export const getTodayForFilename = () => {
+    return DateTime.now().toFormat("yyyy-MM-dd");
+}
+
+export const getYYMMDD = () => {
+    return DateTime.now().toFormat('yyyyMMddHHMMss');
+};
+
+export const epochToDate = (epoch) => {
+    let d  = new Date(0);
+    d.setUTCSeconds(epoch);
+    return d;
+}
+
+console.log("Current timestamp: ", getLatestCycleTimestampFromNow());
