7006388753708e9dc55a56c0bc9cf43e7c65e8b9
John Garnham <john.e.garnham@gmail.com>
2021-07-23 20:37:22 -0600

    Reload bot config after set_prefix. Close #2

--- commands/set_prefix.js
+++ commands/set_prefix.js
@@ -1,5 +1,5 @@
 
-const config = require('../config.json');   // Loads bot config file
+const oldConfig = require('../config.json');   // Loads bot config file
 const fs = require('fs');                   // Loads the Filesystem library
 
 module.exports = {
@@ -11,33 +11,49 @@ module.exports = {
         if(!args.length) {
             // No new prefix. Output usage
             message.channel.send("Usage: " + prefix + "set_prefix <new_prefix>");
+            return;
         } else {
             // Read in new prefix
             newPrefix = args[0];
             // Create new config
             newConfig = {
-                token: config.token,
+                token: oldConfig.token,
                 prefix: newPrefix,
             };
             try {
                 // Write new config 
-                fs.writeFile("config.json", JSON.stringify(newConfig, null, 2), function(err, result) {
+                fs.writeFileSync("config.json", JSON.stringify(newConfig, null, 2), function(err, result) {
                     if (err) throw err;
                 });
-                // Reload config
-                console.log("Setting new prefix to " + newPrefix);
-
-                // Output change
-		        message.channel.send("Set new command prefix to \"" + newPrefix + "\"");
             } catch(e) {
                 console.error("Error writing new config.json: " + e);
                 console.error(e.stack);
                 message.channel.send("Could not set new command prefix: " + e);
             }
 
-            //message.client.destroy();
-            //message.client.login();
+        }
 
+        try {
+            // Reload config
+            console.log("Setting new prefix to " + newPrefix);
+            // Reload config.json here
+            delete require.cache[require.resolve('../config.json')]   // Delete cache
+            config = require("../config.json");
+            // Reload bot
+            message.channel.send("Set new command prefix to \"" + newPrefix + "\"")
+            .then(msg => message.client.destroy())
+            .then(() => message.client.botConfig = config)
+            .then(() => message.client.login(config.token)
+            .then(() => { 
+                let statusMessage = "say " + message.client.botConfig.prefix + "help | Online";
+                // Set the client user's presence
+                message.client.user.setPresence({ activity: { name: statusMessage}, status: "online" })
+                .catch(console.error);
+            }));
+        } catch(e) {
+            console.error("Error reloading bot: " + e);
+            console.error(e.stack);
+            message.channel.send("Could not reload bot with new config.json: " + e);
         }
 	},
 };
