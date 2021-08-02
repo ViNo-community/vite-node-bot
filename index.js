c98b7f487923219a2865920c3aa38223ceea5848
John Garnham <john.e.garnham@gmail.com>
2021-08-02 09:14:59 -0600

    Add handling of unknown commands. Close #20

--- index.js
+++ index.js
@@ -37,13 +37,17 @@ client.on('message', message => {
 	const args = message.content.slice(prefix.length).trim().split(/ +/);
 	const command = args.shift().toLowerCase();
 
-	if (!client.commands.has(command)) return;
+	if (!client.commands.has(command)) {
+		console.error("Unknown command: " + command);
+		message.reply('I do not know what ' + command + ' means');
+		return;
+	} 
 
 	try {
 		client.commands.get(command).execute(message, args);
 	} catch (error) {
 		console.error(error);
-		message.reply('there was an error trying to execute that command!');
+		message.reply('There was an error trying to execute ' + command.name);
 	}
 });
 
