9edf7fe38937a6cf8513b780b8687cf59333584e
John Garnham <john.e.garnham@gmail.com>
2021-09-12 02:52:44 -0600

    Restore !token

--- commands/token.ts
+++ commands/token.ts
@@ -1,4 +1,6 @@
-import { showTokenInformation } from '../tokenHelper';
+import { RPCResponse, TokenInfo } from '@vite/vitejs/distSrc/utils/type';
+import { viteClient } from '../index';
+
 
 module.exports = {
 	name: 'token',
@@ -22,3 +24,38 @@ module.exports = {
         });
 	},
 };
+
+
+const getTokenInformation = async (tokenID: string) => {
+    const tokenInfo: TokenInfo = await viteClient.request('contract_getTokenInfoById', tokenID);
+    return tokenInfo;
+};
+
+const showTokenInformation = async (message, tokenID: string) => {
+
+    let tokenInfo: TokenInfo;
+
+    // Get token info for specified tokenID
+    tokenInfo = await getTokenInformation(tokenID).catch((res: RPCResponse) => {
+        console.log(`Could not retrieve token info for ${tokenID}}`, res);
+        throw res.error;
+    });
+
+    let chatMessage = "";
+    if(tokenInfo == null) {
+        chatMessage = "No token information available for " + tokenID;
+    } else {
+        chatMessage = "**Token Name:** " + tokenInfo.tokenName +
+            "\n**Token Symbol:** " + tokenInfo.tokenSymbol +
+            "\n**Token ID:** " + tokenInfo.tokenId +
+            "\n**Decimals:** " + tokenInfo.decimals +
+            "\n**Owner:** " + tokenInfo.owner +
+            "\n**Is ReIssuable:** " + tokenInfo.isReIssuable +
+            "\n**Max Supply:** " + tokenInfo.maxSupply +
+            "\n**Owner Burn Only:** " + tokenInfo.isOwnerBurnOnly + 
+            "\n**Index:** " + tokenInfo.index;
+    }
+    // Send response to chat
+    message.channel.send(chatMessage);
+
+}
