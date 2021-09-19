0d6673444e4e3820dfd5dcf60f5905b90770e7f6
John Garnham <john.e.garnham@gmail.com>
2021-09-19 08:52:39 -0600

    fix: Chunk !contract msg. Close #28

--- src/commands/contract.ts
+++ src/commands/contract.ts
@@ -6,6 +6,8 @@ import { getLogger } from '../logger';
 
 const logger = getLogger();
 
+const CHUNK_SIZE = 500;
+
 module.exports = {
 	name: 'contract',
 	description: 'Display smart contract information for specified address',
@@ -53,16 +55,43 @@ const showContractInformation = async (message, address: string) => {
     let chatMessage = "";
     if(contractInfo == null) {
         chatMessage = "No smart contract information available for " + address;
-    } else {
-        chatMessage = "**Address:** " + address +
-            "\n**Code:** " + contractInfo.code +
-            "\n**GID:** " + contractInfo.GID +
-            "\n**Random Degree:** " + contractInfo.randomDegree +
-            "\n**Response Latency:** " + contractInfo.responseLatency +
-            "\n**Quota Multiplier:** " + contractInfo.quotaMultiplier
+        // Send response to chat
+        logger.info(chatMessage);
+        message.channel.send(chatMessage);
+        return;
     }
+
+    // Construct chat message
+    chatMessage = "**Address:** " + address +
+        "\n**GID:** " + contractInfo.GID +
+        "\n**Random Degree:** " + contractInfo.randomDegree +
+        "\n**Response Latency:** " + contractInfo.responseLatency +
+        "\n**Quota Multiplier:** " + contractInfo.quotaMultiplier;
     // Send response to chat
-    logger.info(chatMessage);
+    logger.info(chatMessage)
     message.channel.send(chatMessage);
+    // Send code part of message
+    chatMessage = "\n**Code:** " + contractInfo.code;
+    // Message may be too large for DISCORD to handle, so split into CHUNK_SIZE sized 
+    // chunks and send it to Discord one chunk at a time
+    var chunks = chunkString(chatMessage, CHUNK_SIZE);
+    for (const chunk of chunks) { // You can use `let` instead of `const` if you like
+        // Send response to chat
+        logger.info(chunk);
+        message.channel.send(chunk);
+    }
+
 
 }
+
+// Split str up into size sized chunks
+function chunkString(str, size) {
+    const numChunks = Math.ceil(str.length / size)
+    const chunks = new Array(numChunks)
+  
+    for (let i = 0, o = 0; i < numChunks; ++i, o += size) {
+      chunks[i] = str.substr(o, size)
+    }
+  
+    return chunks
+  }
